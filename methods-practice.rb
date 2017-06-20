376041b4103c3c53973a5158d13eed8786913cfb
Jai <jpday140@gmail.com>
2017-06-20 19:10:02 +0000

    Finished the lab

---
+++ methods-practice.rb
@@ -0,0 +1,21 @@
+def timesByOneThousand(number, number2)
+     number*number2
+end 
+timesByOneThousand(5.0, 1000)
+
+def howOldIWillBeIn2030(birthyear, thirty)
+    thirty-birthyear
+end    
+howOldIWillBeIn2030(2000, 2030)
+
+def makeExcited(excited)
+    "hello".upcase + "!!!"
+end
+
+makeExcited("hello")
+
+def happyBackwards(happy)
+    "cool".reverse + ":)"
+end
+puts happyBackwards("cool")
+
